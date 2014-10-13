6b6755a9bd8d530737d28301521208c9d6b48f99
Jonn Mostovoy <amarr.industrial@gmail.com>
2014-10-13 23:54:16 +0200

    Add F7 bundle, configure webpack

---
+++ entry.js
@@ -0,0 +1,5 @@
+require('react');
+require('./js/framework7.js');
+require('./css/framework7.css');
+require('./css/ionicons.css');
+console.log('Required react');
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './entry.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css.*/, loader: "style!css" },
+      { test: /\.woff.*/, loader: "url-loader?mimetype=application/x-font-woff" },
+      { test: /\.eot.*/, loader: "url-loader?mimetype=application/x-font-eot" },
+      { test: /\.ttf.*/, loader: "url-loader?mimetype=application/x-font-ttf" },
+      { test: /\.png.*/, loader: "url-loader?mimetype=image/png" },
+      { test: /\.svg.*/, loader: "url-loader?mimetype=image/svg" }
+    ],
+  }
+};
